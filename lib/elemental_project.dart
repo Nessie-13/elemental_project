0b78eb3db0a299d43af614f6446e32ef11f396bf
Nessie-13 <121945825+Nessie-13@users.noreply.github.com>
2023-12-29 17:08:01 +0000

    added camera component

--- lib/elemental_project.dart
+++ lib/elemental_project.dart
@@ -1,12 +1,21 @@
 import 'dart:async';
 
-import 'package:elemental_project/earth_levels/earth.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:elemental_project/earth_levels/earth.dart';
+
 
 class ElementalProject extends FlameGame {
+  late final CameraComponent cam;
+
+  final world = level();
+
+
   @override
   FutureOr<void> onLoad() {
-    add(Earth1());
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    addAll([cam, world]);
     return super.onLoad();
   }
 }
